01f3f63ea7f06ddc5b1c676fd3bc7a80845e4f29
rohit basu <robasu@paypal.com>
2018-12-19 16:26:45 -0800

    First Commit

---
+++ src/pages/index.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default () => (
+    <div style={{color: `purple`}}>
+        <h2>The First Gatsby Page</h2>
+        <p>This is a sub-module in BY_EXAMPLE Series</p>
+        <img src="https://source.unsplash.com/random/400x200" alt="a random image" />{" "}
+    </div>
+)
