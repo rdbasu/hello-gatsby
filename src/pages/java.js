e3c46958a2a08f880a52bf5158a100e2816a156a
rohit basu <robasu@paypal.com>
2019-02-12 11:42:30 -0800

    change

--- src/pages/java.js
+++ src/pages/java.js
@@ -5,7 +5,7 @@ import { graphql } from "gatsby"
 
 export default ({ data }) => (
   <Layout>
-    <h1>{data.site.siteMetadata.title}</h1>
+    <h1>blah blh {data.site.siteMetadata.title}</h1>
     <Header headerText={data.site.siteMetadata.java} />
   </Layout>
 )
