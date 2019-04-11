dd396230e5e4c1a15a9287ccd58cfe8695e812fd
Rohit Basu <robasu@paypal.com>
2019-04-11 13:40:41 -0700

    more changes

---
+++ src/pages/testonemore.md
@@ -0,0 +1,8 @@
+---
+title: "new title"
+---
+
+# new content
+
+- a
+- b
--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,9 +1,10 @@
 import Typography from "typography"
 // import bootstrapTheme from "typography-theme-bootstrap"
-// import stardustTheme from "typography-theme-stardust"
+import stardustTheme from "typography-theme-stardust"
 import typographythemefunston from "typography-theme-funston"
 // const typography = new Typography({
 //     baseFontSize: "16px"
 // })
+// const typography = new Typography(stardustTheme)
 const typography = new Typography(typographythemefunston)
 export default typography
